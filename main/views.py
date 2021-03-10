42f6b6012cc69175b59e0b986bda610d7cc0ed94
abdulqodirumarov <kodir1999@mail.ru>
2021-03-10 11:20:34 +0500

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Main Ueb page', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
