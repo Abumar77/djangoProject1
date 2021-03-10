dc5ecec39ffe319769b4f340900227c3e698a2f5
abdulqodirumarov <kodir1999@mail.ru>
2021-03-10 13:01:20 +0500

    Before adding creating functions

--- main/views.py
+++ main/views.py
@@ -3,9 +3,13 @@ from .models import Task
 
 
 def index(request):
-    tasks = Task.objects.all()
-    return render(request, 'main/index.html', {'title': 'Main Ueb page', 'tasks': tasks})
+    tasks = Task.objects.order_by("-id")[:5]
+    return render(request, 'main/index.html', {'title': 'Main Web page', 'tasks': tasks})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def create(request):
+    return render(request, 'main/create.html')
